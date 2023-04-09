9e289adf91e26e16a7ca4d949087fe5a2b2d7d6a
Yogeshdhengale <yd30668497@gmail.com>
2023-04-09 23:46:45 +0530

    not working proper
    Co-Authored-By: nge <682267+nge@users.noreply.github.com>

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,15 @@
 const express=require('express')
 require('dotenv').config();
+const cors=require('cors')
 const PORT=process.env.PORT || 5000
 const dbconnect=require('./connection/connection')
-const cors=require("cors")
-const app=express()
-app.use(cors())
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, author");
-  next();
-});
 
+const app=express()
+app.use(cors({
+    origin: 'http://localhost:3000'
+  }));
+
+app.options('*', cors());
 
 
 dbconnect()
