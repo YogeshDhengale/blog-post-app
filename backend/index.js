1c810af1f9a9b57c8c6d0a758a300cf2b5c8b843
Yogeshdhengale <yd30668497@gmail.com>
2023-04-09 23:37:06 +0530

    cors
    Co-Authored-By: nge <682267+nge@users.noreply.github.com>

--- backend/index.js
+++ backend/index.js
@@ -2,9 +2,13 @@ const express=require('express')
 require('dotenv').config();
 const PORT=process.env.PORT || 5000
 const dbconnect=require('./connection/connection')
-
+const cors=require("cors")
 const app=express()
-
+app.use(cors())
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, author");
+  next();
+});
 
 
 
