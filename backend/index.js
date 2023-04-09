e8ad31dd2f534b2d18950c9be253679f984ead08
Yogeshdhengale <yd30668497@gmail.com>
2023-04-09 21:33:25 +0530

    change

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,16 @@
 const express=require('express')
 require('dotenv').config();
-const PORT=process.env.PORT || 5000
 const dbconnect=require('./connection/connection')
 const cors=require("cors")
 
-const app=express()
-app.use(cors())
-
+const PORT=process.env.PORT || 5000
 dbconnect()
+
+const app=express()
 
 const registerAndLogin=require('./routes/registerAndLogin')
 const addblog=require('./routes/blog')
-
+app.use(cors())
 app.use(registerAndLogin)
 app.use(addblog)
 
