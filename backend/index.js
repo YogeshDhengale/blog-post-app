4ac24cb8d5ce3517e9931f587471ed3f23556f6e
Yogeshdhengale <yd30668497@gmail.com>
2023-04-09 21:59:48 +0530

    cors changed

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,6 @@
 const express=require('express')
 require('dotenv').config();
 const dbconnect=require('./connection/connection')
-const cors=require("cors")
 
 const PORT=process.env.PORT || 5000
 dbconnect()
@@ -10,7 +9,7 @@ const app=express()
 
 const registerAndLogin=require('./routes/registerAndLogin')
 const addblog=require('./routes/blog')
-app.use(cors());
+
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
