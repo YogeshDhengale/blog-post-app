7de415942082ca80cce3faeded2c0629c4750ef2
Yogeshdhengale <yd30668497@gmail.com>
2023-04-09 21:45:47 +0530

    cors change due to the error

--- backend/index.js
+++ backend/index.js
@@ -11,6 +11,15 @@ const app=express()
 const registerAndLogin=require('./routes/registerAndLogin')
 const addblog=require('./routes/blog')
 app.use(cors())
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'https://6432de01c1587d3e2bd1e14c--reliable-dusk-a300d2.netlify.app');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+  next();
+});
 app.use(registerAndLogin)
 app.use(addblog)
 
